8d7e43c3e4fb4368ed53237e610f60670bae396f
Jorge Pando <jpandoji@alumnos.unex.es>
2022-03-01 13:35:49 +0100

    Implementacion clase Main probando metodos

--- src/MainGit.java
+++ src/MainGit.java
@@ -5,5 +5,8 @@ public class MainGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo Java con Git");
+		Scanner sc = new Scanner(System.in);
+		Suma.sumar(5, 3);
+		Suma.sumarAcum(10);
 	}
 }
