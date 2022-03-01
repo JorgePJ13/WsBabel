09e2503119548bf1028d86df448764f553fb1c9a
Jorge Pando <jpandoji@alumnos.unex.es>
2022-03-01 13:40:55 +0100

    Implementacion Main probando metodos resta

--- src/MainGit.java
+++ src/MainGit.java
@@ -6,7 +6,13 @@ public class MainGit {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo Java con Git");
 		Scanner sc = new Scanner(System.in);
+		
+		// Metodos Clase Suma
 		Suma.sumar(5, 3);
 		Suma.sumarAcum(10);
+		
+		// Metodos Clase Resta
+		Resta.restar(9, 7);
+		Resta.restaAcum(5);
 	}
 }
