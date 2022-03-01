4cfa4deeb53c7bbe12dc4a1495d922fe8d309ab8
Jorge Pando <jpandoji@alumnos.unex.es>
2022-03-01 13:34:39 +0100

    Implementacion clase Suma

---
+++ src/MainGit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+
+public class MainGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo Java con Git");
+	}
+}
---
+++ src/Suma.java
@@ -0,0 +1,14 @@
+
+public class Suma {
+
+	public static void sumar(int numero1, int numero2) {
+		int resultado = numero1 + numero2;
+		System.out.println("La suma es: " +resultado);
+	}
+	
+	public static void sumarAcum(int valor) {
+		int suma = 5;
+		suma+= valor;
+		System.out.println("La suma acumulada es: " +suma);
+	}
+}
